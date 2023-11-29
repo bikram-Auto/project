9eeb662aa7447113719c5e014cab8dc97177b7fe
Bikram <bikram.nanda@squbix.com>
2023-11-29 12:17:04 +0530

    copy/past body

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from "@nestjs/common";
+import { Controller, Get, Post, Req } from "@nestjs/common";
 
 
 @Controller()
@@ -9,7 +9,7 @@ export class AppController{
     }
 
     @Post('/user')
-    postUser(){
-        return 'it storing';
+    postUser(@Req() req: Request){
+        return req.body;
     }
 }

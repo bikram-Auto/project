c2c1e385716d3718b361abad3ad8711da898e15c
Bikram <bikram.nanda@squbix.com>
2023-11-30 09:56:56 +0530

    set server for mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
+// import { AppController } from "./app.controller";
 import { UserModule } from './user/user.module';
+// import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-    // controllers: [AppController],
-    imports: [UserModule],
+    
+    imports: [
+        UserModule,
+        MongooseModule.forRoot('mongodb://localhost:27017/')
+    ],
 })
 export class AppModule{}
